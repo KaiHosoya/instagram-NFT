dca96535e6eec2ec74acaa13d0b5bdb3ae933bf8
KaiHosoya <kaihosoya711@gmail.com>
2022-11-07 22:51:38 +0900

    contractの実装

--- chain/hardhat.config.js
+++ chain/hardhat.config.js
@@ -1,4 +1,17 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.17",
-};
+   solidity: "0.8.4",
+   defaultNetwork: "goerli",
+   networks: {
+      hardhat: {},
+      goerli: {
+         url: ALCHEMY_API_URL,
+         accounts: [`0x${METAMASK_PRIVATE_KEY}`]
+      }
+   },
+}
